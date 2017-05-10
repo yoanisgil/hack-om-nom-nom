fb98e5a751eb0987fbc5cbf29f8ace395551cdb8
Jeffrey Rainy <jeff@elementai.com>
2017-05-10 11:12:08 -0400

    own index

--- random_player.py
+++ random_player.py
@@ -5,11 +5,14 @@ class Player(object):
     def __init__(self):
         pass
 
-    def init(self):
+    def init(self, index):
+        self.my_index = index
         pass
     
     def play(self, state, cards_to_play):
         card = random.choice(cards_to_play)
+
+        print("Payouts are {}".format(state.payouts))
         print("Playing {}".format(card))
         return card
 
