8f42d6a045c3bc70f7d638e0ebec682cd7311f56
Jeffrey Rainy <jeff@elementai.com>
2017-05-10 11:02:40 -0400

    hooking it up

--- random_player.py
+++ random_player.py
@@ -2,5 +2,20 @@ import random
 
 
 class Player(object):
+    def __init__(self):
+        pass
+
+    def init(self):
+        pass
+    
     def play(self, state, cards_to_play):
-        return random.choice(cards_to_play)
+        card = random.choice(cards_to_play)
+        print("Playing {}".format(card))
+        return card
+
+    def wasPlayed(self):
+        pass
+
+    def roundEnded(self):
+        pass
+
