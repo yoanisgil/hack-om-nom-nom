1596d33bff104155a3b1c0052ee5b0c8213be575
Jeffrey Rainy <jeff@elementai.com>
2017-05-10 12:56:33 -0400

    small steps

--- ai_player.py
+++ ai_player.py
@@ -1,15 +1,16 @@
 import random
+import copy
 import tensorflow as tf
+from game_logic import State, new_state
+import itertools
 
 EMBED_DIM = 10
 
 class Evaluator:
     def __init__(self):
-    
-        print("Evaluator init")
-    
+
         learning_rate = 0.1
-    
+
         self.payouts = tf.placeholder(tf.int32, [None, 6]) # the payouts
         self.cards = tf.placeholder("float", [None, 6]) # whether I still have the card
 
@@ -22,24 +23,25 @@ class Evaluator:
             'b2': tf.Variable(tf.random_normal([6])),
             }
 
+    def evaluate(self):
+        return([1/6.0, 1/6.0, 1/6.0, 1/6.0, 1/6.0, 1/6.0])
 
 
 class AiPlayer(object):
     def __init__(self):
-    
-        self.eval = Evaluator()
         pass
 
     def init(self, index, total_players):
         self.my_index = index
-        pass
+        self.total_players = total_players
+
+        self.evals = [Evaluator() for x in xrange(self.total_players)]
 
     def play(self, state, cards_to_play):
-        card = random.choice(cards_to_play)
+    
+        strategies = itertools.permutations(cards_to_play)
 
-        print("Payouts are {}".format(state.payouts))
-        print("State.cards is {}".format(state.cards))
-        print("Playing {}".format(card))
+        card = random.choice(cards_to_play)
         return card
 
     def turn_ended(self, moves):
