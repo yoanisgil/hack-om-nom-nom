5bf44307d9b7629f94339addc3947ead5aeedd00
Yoanis Gil <ygil@elementai.com>
2017-05-10 15:24:59 -0400

    Add more tests

--- tests/test_new_state.py
+++ tests/test_new_state.py
@@ -109,3 +109,38 @@ def test_one_predator_one_prey_one_dice():
     assert new_state.dices_distribution[-1] == 14
     assert new_state.cards[0] == [1, 2, 3, 4, 5]
     assert new_state.cards[1] == [0, 1, 2, 4, 5]
+
+def test_two_predators_two_preys_one_dice():
+    # All dices are in the last cell
+    dices_distribution = [0] * 9
+    dices_distribution[3] = 1
+    dices_distribution[-1] = 14
+
+    state = State.from__config(4, dices_distribution)
+
+    new_state = next_state(state, [0, 3, 0, 3])
+
+    assert new_state.score == [2, 0, 2, 0]
+    assert new_state.dices_distribution[3] == 1
+    assert new_state.dices_distribution[-1] == 14
+    assert new_state.cards[0] == [1, 2, 3, 4, 5]
+    assert new_state.cards[1] == [0, 1, 2, 4, 5]
+    assert new_state.cards[2] == [1, 2, 3, 4, 5]
+    assert new_state.cards[3] == [0, 1, 2, 4, 5]
+
+def test_two_predators_one_prey_two_dice():
+    # All dices are in the last cell
+    dices_distribution = [0] * 9
+    dices_distribution[3] = 2
+    dices_distribution[-1] = 13
+
+    state = State.from__config(3, dices_distribution)
+
+    new_state = next_state(state, [0, 3, 0])
+
+    assert new_state.score == [2, 0, 2]
+    assert new_state.dices_distribution[3] == 1
+    assert new_state.dices_distribution[-1] == 13
+    assert new_state.cards[0] == [1, 2, 3, 4, 5]
+    assert new_state.cards[1] == [0, 1, 2, 4, 5]
+    assert new_state.cards[2] == [1, 2, 3, 4, 5]
