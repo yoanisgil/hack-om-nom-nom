d0503e0aa51c65edd8b280c08ad92c31fcfc18ad
Jeffrey Rainy <jeff@elementai.com>
2017-05-10 14:22:11 -0400

    full tree search with sampling

--- ai_player.py
+++ ai_player.py
@@ -5,6 +5,7 @@ from game_logic import State, new_state
 import itertools
 
 EMBED_DIM = 10
+SAMPLES = 20
 
 class Evaluator:
     def __init__(self):
@@ -31,18 +32,60 @@ class AiPlayer(object):
     def __init__(self):
         pass
 
+    # init the AI player
     def init(self, index, total_players):
         self.my_index = index
         self.total_players = total_players
 
         self.evals = [Evaluator() for x in xrange(self.total_players)]
 
+    def valueFromScore(self, score):
+        if max(score) == score[self.my_index]:
+            return 1.0
+        else:
+            return 0.0
+
+    # internal function to sample a strategy and evaluate the score
+    def sample(self, state, strategy):
+
+        # play my moves in sequence
+        for x in strategy:
+
+            moves = [random.choice(y) for y in state.cards] # pick a random card, in each player possible cards. TODO : samples
+            moves[self.my_index] = x
+
+            state = new_state(state, moves)
+
+    # API call to play a move
     def play(self, state, cards_to_play):
-    
+
         strategies = itertools.permutations(cards_to_play)
 
-        card = random.choice(cards_to_play)
-        return card
+        strategy_value = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        strategy_value_count = [0, 0, 0, 0, 0, 0]
+
+        for strategy in strategies:
+            for i in xrange(SAMPLES):
+                next_state = copy.copy(state)
+                self.sample(next_state, strategy)
+
+            # the value of the strategy starting by each move is a function of the score
+            strategy_value[strategy[0]] = strategy_value[strategy[0]] + self.valueFromScore(next_state.score)
+            strategy_value_count[strategy[0]] = strategy_value_count[strategy[0]] + 1
+
+        bestValue = 0.0
+        bestCard = cards_to_play[0]
+
+        for x in xrange(6):
+            if strategy_value_count > 0:
+                strategy_value[x] = strategy_value[x] / strategy_value_count[x]
+            if strategy_value[x] > bestValue:
+                bestValue = strategy_value[x]
+                bestCard = x
+
+        print("playing card {}".format(bestCard))
+
+        return bestCard
 
     def turn_ended(self, moves):
         pass
