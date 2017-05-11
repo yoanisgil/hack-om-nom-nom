65df28ceb9f14616d67dd746df79a78e6b58387a
Nathan Schucher <nathan.schucher@gmail.com>
2017-05-11 12:46:29 -0400

    Add playable cards to UI

--- api.py
+++ api.py
@@ -11,9 +11,18 @@ game_engine = WebGameEngine()
 
 @app.route('/')
 def index():
+    cards = [ "Cat", "Hedgehog", "Wolf", "Rat", "Frog", "Rabbit" ]
+    card_imgs = [ 
+            "https://pbs.twimg.com/profile_images/737359467742912512/t_pzvyZZ_400x400.jpg",
+            "https://pbs.twimg.com/profile_images/588180948887277568/qURfGz0F.jpg", 
+            "http://study.com/cimages/multimages/16/gray_wolf_5584759091_wikimedia.jpg",
+            "https://static.wixstatic.com/media/d5911b_d49cde5834a5486aad795ce2c1e08d8e.jpg_256",
+            "https://pbs.twimg.com/profile_images/639120110913912832/gkb4kJs0_400x400.jpg",
+            "https://pbs.twimg.com/profile_images/590950028753117184/L5pk9F7z.png"
+            ]
     game_tiles = [['Cat', 'Rat', 'Cheese'], ['Hedgehog', 'Frog', 'Fly'], ['Wolf', 'Rabbit', 'Carrot']]
     transposed = map(list, zip(*game_tiles))
-    return render_template('game.html', game_tiles=transposed)
+    return render_template('game.html', cards=zip(cards, card_imgs), game_tiles=transposed)
 
 
 @app.route('/init_session', methods=['POST'])
